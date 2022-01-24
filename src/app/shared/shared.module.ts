52a69c7ed13441afdb6af370e1631e7e6b0efd23
Ivan dos Santos <ivantkd1@gmail.com>
2022-01-24 17:07:43 -0300

    Create header, slider and init section about

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { SliderComponent } from './components/slider/slider.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BannerComponent } from './components/banner/banner.component';
+
+@NgModule({
+  declarations: [BannerComponent, SliderComponent],
+  exports: [BannerComponent, SliderComponent],
+  imports: [CommonModule],
+})
+export class SharedModule {}
